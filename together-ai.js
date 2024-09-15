2a0edac8c0659f9f5db142a4cb5d40ad28679024
Michael Wood <michaelwood33311@icloud.com>
2024-09-15 11:10:48 -0400

    added completions function

---
+++ together-ai.js
@@ -0,0 +1,60 @@
+require ('dotenv').config();
+const lodash = require('lodash');
+const axios = require('axios');
+
+const { TOGETHER_API_TOKEN } = process.env;
+
+const togetherAiHeaders = () => {
+    return {
+        accept: 'application/json',
+        authorization: 'Bearer ' + TOGETHER_API_TOKEN,
+        "content-type": 'application/json'
+    }
+}
+
+const chatCompletions = async (model, messages, options = {}) => {
+
+    const endpoint = 'https://api.together.xyz/v1/chat/completions';
+
+    const request = {
+        url: endpoint,
+        method: 'post',
+        headers: togetherAiHeaders(),
+        data: {
+            messages,
+            model
+        }
+    }
+
+    lodash.merge(request.data, options);
+ 
+    try {
+        const response = await axios(request);
+        const usage = response?.data?.usage;
+        const { prompt_tokens, completion_tokens, total_tokens } = usage;
+        console.log(usage);
+        return response?.data?.choices[0]?.message?.content;
+    } catch (err) {
+        console.error(err.response.data);
+        return false;
+    }
+
+}
+
+const test = async () => {
+    const info = await chatCompletions('mistralai/Mixtral-8x7B-Instruct-v0.1', 
+        [
+            {
+              "role": "system",
+              "content": "You are a helpful assistant"
+            },
+            {
+              "role": "user",
+              "content": "What is 1 + 1?"
+            }
+        ]);
+
+    console.log(info);
+}
+
+test();
